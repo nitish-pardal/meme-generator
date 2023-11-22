823169b8825b80ba6498152f5479eff382cd1be3
nitish-pardal <nitishpardal@gmail.com>
2023-11-22 20:51:18 +0530

    initial commit

---
+++ meme-generator/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header(){
+return(
+  <header className="header-container">
+    <img src="/assets/TrollFace.png" alt="troll-face" className="troll-image" />
+    <h3 className="heading">Meme Generator</h3>
+    <p className="project-info">project-3</p>
+  </header>
+    )
+}
---
+++ meme-generator/src/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+import Header from "../components/Header"
+export default function App(){
+  return(
+  <Header/>
+)}
