6a509fde459543c80af916050b73fa4442264500
nitish-pardal <nitishpardal@gmail.com>
2023-11-24 20:37:27 +0530

    24-11-2023

--- meme-generator/components/Main.jsx
+++ meme-generator/components/Main.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import  ReactDOM  from "react";
-
+import data from "./memesdata.jsx"
 export default function Main(){
+
+  const [memeImage,setMemeImage]= React.useState("");
+  const ImageArray=data.data.memes;
+  const length =ImageArray.length;
+  function handleClick(){
+    const randomNumber =Math.floor(Math.random()*length);
+    setMemeImage(ImageArray[randomNumber].url);
+  }
+
   return (
     <div className="input-container">
       <input  className="text-container" type="text" placeholder="first-name"/>
       <input className="text-container"  type="text" placeholder="last-name"/>
-      <button className="meme-button">Get a new meme image  🖼</button>
+      <button className="meme-button" onClick={handleClick}>Get a new meme image  🖼</button>
+      <img src= {memeImage} alt="random-meme-image" className="meme--image"/>
     </div>
   )
 }
--- meme-generator/src/App.jsx
+++ meme-generator/src/App.jsx
@@ -3,6 +3,7 @@ import  ReactDOM  from "react-dom/client";
 import Header from "../components/Header";
 import Main from "../components/Main";
 export default function App(){
+
   return(
   <div>
     <Header/>
